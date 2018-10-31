d8fcdb00924a01ed93f54886f94b9c9fca17fff4
Erik Arvidsson <e@bs61c-282e436f1.staden.gotheborg.net>
2018-10-31 13:27:32 +0100

    reed to read
    Spell fix

--- authors.php
+++ authors.php
@@ -14,7 +14,7 @@
             <?php foreach ($authors as $author) : ?>
                 <br>
                 <a class="col-4" href="authorarticle.php?author=<?= $author['name']; ?>" style="color: black; text-decoration: none;">
-                    <h2 style="justify-content: center; text-align: center;"> Reed alla articels by: </h2>
+                    <h2 style="justify-content: center; text-align: center;"> Read alla articels by: </h2>
                     <div class="container" style="justify-content: center; text-align: center;">
                         <img class="img-fluid" src="<?= $author['img']; ?>" alt="Author name" style="border-radius: 50%; width: 250px; max-width: 100%;">
                         <h2 style="justify-content: center; text-align: center;"> <?= $author['name']; ?> </h2>
